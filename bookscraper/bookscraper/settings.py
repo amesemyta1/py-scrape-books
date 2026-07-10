0b2854eb53a5452f9e1f21379d2b6bddd4d6d6e4
amesemyta1 <amesemyta@gmail.com>
2026-07-10 18:17:37 +0300

    Add scrapy spider for books.toscrape.com and scraped data

---
+++ bookscraper/bookscraper/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = "bookscraper"
+
+SPIDER_MODULES = ["bookscraper.spiders"]
+NEWSPIDER_MODULE = "bookscraper.spiders"
+
+ADDONS = {}
+
+ROBOTSTXT_OBEY = True
+
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+DOWNLOAD_DELAY = 1
+
+FEED_EXPORT_ENCODING = "utf-8"
---
+++ bookscraper/bookscraper/spiders/books_spider.py
@@ -0,0 +1,49 @@
+import scrapy
+from scrapy.http import Response
+from typing import Iterable, Any
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+
+    def parse(self, response: Response) -> Iterable[scrapy.Request]:
+        books = response.css("article.product_pod h3 a::attr(href)").getall()
+
+        for book_url in books:
+            yield response.follow(book_url, callback=self.parse_book)
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+    def parse_book(self, response: Response) -> Iterable[dict[str, Any]]:
+        rating_map = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+        rating_class = response.css("p.star-rating::attr(class)").get()
+        rating = None
+
+        if rating_class:
+            rating_word = rating_class.split(" ")[-1]
+            rating = rating_map.get(rating_word)
+
+        stock_text = response.css(
+            "p.instock.availability::text"
+        ).re_first(r"\d+")
+        amount_in_stock = int(stock_text) if stock_text else 0
+
+        yield {
+            "title": response.css("div.product_main h1::text").get(),
+            "price": response.css("p.price_color::text").get(),
+            "amount_in_stock": amount_in_stock,
+            "rating": rating,
+            "category": response.xpath(
+                '//ul[@class="breadcrumb"]/li[3]/a/text()'
+            ).get(),
+            "description": response.xpath(
+                '//div[@id="product_description"]/following-sibling::p/text()'
+            ).get(),
+            "upc": response.xpath(
+                '//th[text()="UPC"]/following-sibling::td/text()'
+            ).get(),
+        }
